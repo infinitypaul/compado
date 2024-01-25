680ed04048365b05ee6051243b482286829948d5
infinitypaul <infinitypaul@live.com>
2024-01-25 19:36:43 +0100

    Initialize Compado Product List plugin with composer autoload

---
+++ compado-product-list.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Compado Product List
+ * Plugin URI:  https://infinitypaul.medium.com
+ * Description: Fetches and displays a list of products from Compado's API.
+ * Version:     1.0.0
+ * Author:      Paul Edward
+ * Author URI:  https://infinitypaul.medium.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: compado-product-list
+ * Domain Path:       /languages
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
