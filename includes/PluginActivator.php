b7b42a0c8f6c26935aa32a663d8803580320df66
infinitypaul <infinitypaul@live.com>
2024-01-26 02:58:44 +0100

    Updated Compado product list plugin activation methods

--- includes/PluginActivator.php
+++ includes/PluginActivator.php
@@ -4,12 +4,16 @@ namespace Compado\Products;
 
 class PluginActivator
 {
-    public static function activate() {
+    public static function activate(): void
+    {
         add_rewrite_rule('^compado-redirect/(.+)', 'index.php?compado_redirect=$matches[1]', 'top');
         flush_rewrite_rules();
     }
 
-    public static function deactivate() {
+    public static function deactivate(): void
+    {
         flush_rewrite_rules();
+        delete_transient(ApiClient::TRANSIENT_KEY);
+        // delete_option('compado_products_options');
     }
 }
