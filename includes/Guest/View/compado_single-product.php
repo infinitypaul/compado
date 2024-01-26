180a0ccaf468a4535eb2ec09aefbed392f0285ed
infinitypaul <infinitypaul@live.com>
2024-01-26 13:32:35 +0100

    Update styles and structure in Compado product list plugin

--- includes/Guest/View/compado_single-product.php
+++ includes/Guest/View/compado_single-product.php
@@ -1,36 +1,123 @@
 <?php if (!isset($product)) return;
-$redirectUrl = home_url('/compado-redirect/') . urldecode($product['api_exitover_url']);
-
+$redirectUrl = home_url('/compado-redirect/') . urlencode($product['api_exitover_url']);
 ?>
 
-<div class="compado-product">
-    <div class="header">Today's #1 Meal Delivery Service!</div>
-
-    <div class="main-content">
-        <img src="<?php echo esc_url($product['logo_image']); ?>" alt="<?php echo esc_attr($product['partner_name']); ?>" class="logo">
-        <div>
-            <h1><?php echo esc_html($product['partner_name']); ?></h1>
-            <p>Create unique & delicious meals with <?php echo esc_html($product['partner_name']); ?>'s easy-to-use meal kits delivered right to you.</p>
-            <div class="offer">Get 60% off 1st box, 25% off next 8 boxes + free treats for life!</div>
-        </div>
-        <div class="rating">
-            <?php for ($i = 0; $i < 10; $i++): ?>
-                <img src="<?php echo $i < $product['rating'] ? 'path_to_full_star.png' : 'path_to_empty_star.png'; ?>" alt="Star" class="star">
-            <?php endfor; ?>
-            <span><?php echo esc_html($product['rating']); ?></span>
-        </div>
+<div class="compado-container">
+    <div class="compado-top">
+        <small class="">Today's #1 Meal Delivery Service!</small>
     </div>
 
-    <?php if (isset($product['info_icons'])): ?>
-        <div class="info-icons">
-            <?php foreach ($product['info_icons'] as $icon): ?>
-                <img src="<?php echo esc_url($icon); ?>" alt="Icon description">
-            <?php endforeach; ?>
+    <div class="compado-inner">
+        <div class="compado-img-container">
+            <img
+                    src="https://media.api-domain-compado.com/media/phplC7VVf.jpg?d=200x120&q=100"
+                    alt=""
+                    class="img"
+            />
         </div>
-    <?php endif; ?>
-
-    <div class="buttons">
-        <button class="button" onclick="location.href='<?php echo esc_url($redirectUrl); ?>'">View Plan</button>
-        <button class="button">Read More</button>
+        <div class="compado-text-container">
+            <h2 class="compado-service-title">HelloFresh!</h2>
+            <p class="compado-service-description">
+                Create unique & delicious meals with Hello Fresh's easy-to-use meal
+                kits delivered right to you.
+            </p>
+            <div class="compado-info-box">
+                <a href="#" class="compado-info-link"
+                >Get 60% off 1st box, 25% off next 8 boxes + free treats for
+                    life!</a
+                >
+            </div>
+        </div>
+        <div class="compado-right">
+            <span class="compado-rating">9.9</span>
+            <span class="compado-stars">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+                <!-- Add star icon -->
+          </span>
+        </div>
+    </div>
+    <div class="compado-bottom">
+        <div class="compado-icons">
+            <div class="compado-icon-container">
+                <img
+                        src="https://media.api-domain-compado.com/img/icons/partner-icons/mealkits.svg?q=100&d=32x32&color=686769"
+                        alt=""
+                />
+                <span class="compado-icon-text">Meal Kits</span>
+            </div>
+            <div class="compado-icon-container">
+                <img
+                        src="https://media.api-domain-compado.com/img/icons/partner-icons/vegetarian.svg?q=100&d=32x32&color=686769"
+                        alt=""
+                />
+                <span class="compado-icon-text">Vegetarian</span>
+            </div>
+            <div class="compado-icon-container">
+                <img
+                        src="https://media.api-domain-compado.com/img/icons/partner-icons/diabetics.svg?q=100&d=32x32&color=686769"
+                        alt=""
+                />
+                <span class="compado-icon-text">Diabetics</span>
+            </div>
+            <div class="compado-icon-container">
+                <img
+                        src="https://media.api-domain-compado.com/img/icons/partner-icons/singles.svg?q=100&d=32x32&color=686769"
+                        alt=""
+                />
+                <span class="compado-icon-text">Singles</span>
+            </div>
+            <div class="compado-icon-container">
+                <img
+                        src="https://media.api-domain-compado.com/img/icons/partner-icons/mastercard.svg?q=100&d=32x32&color=686769"
+                        alt=""
+                />
+                <span class="compado-icon-text">Mastercard</span>
+            </div>
+            <div class="compado-icon-container">
+                <img
+                        src="https://media.api-domain-compado.com/img/icons/partner-icons/paypal.svg?q=100&d=32x32&color=686769"
+                        alt=""
+                />
+                <span class="compado-icon-text">Paypal</span>
+            </div>
+            <div class="compado-icon-container">
+                <span class="compado-text-icon">+1</span>
+                <span class="compado-icon-text">More</span>
+            </div>
+        </div>
+        <button class="compado-plan-btn">View Plan</button>
+    </div>
+    <div class="compado-hidden-container">
+        <ul class="list">
+            <li class="item">
+                <i class="fa fa-check"></i>
+                Save time & money, avoid long lines & high prices at supermarkets
+            </li>
+            <li class="item">
+                <i class="fa fa-check"></i>
+                Personalise your plan to deliver the food you want when you want
+            </li>
+            <li class="item">
+                <i class="fa fa-check"></i>
+                Make changes at any time using HelloFresh's user-friendly app
+            </li>
+        </ul>
+        <div class="compado-img-container">
+            <img
+                    src="https://media.api-domain-compado.com/media/phpnQRJxy.jpg"
+                    alt=""
+                    class="img"
+            />
+        </div>
+    </div>
+    <div class="compado-read-more">
+        <a href="#" class="compado-read-more-link"
+        >Read More
+            <i class="fa fa-chevron-down"></i>
+        </a>
     </div>
 </div>
