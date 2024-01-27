1c101578f990cb57e72828a86e52ca03f7cc4324
infinitypaul <infinitypaul@live.com>
2024-01-27 16:40:27 +0100

    Set Compado API endpoint to a constant

--- includes/Admin/View/compado_api_endpoint_callback.php
+++ includes/Admin/View/compado_api_endpoint_callback.php
@@ -5,5 +5,4 @@ $api_endpoint = $options['api_endpoint'] ?? '';
 
 <div class="compado-field">
     <span id="compado_api_endpoint"><?php echo esc_html($api_endpoint); ?></span>
-<!--    <input type="text" id="compado_api_endpoint" readonly name="compado_products_options[api_endpoint]" value="--><?php //echo esc_attr($api_endpoint); ?><!--" size="50">-->
 </div>
